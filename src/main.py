a5756615840741eb6b217543219ea17c195d98b4
EmelyanovAlexey <43723067+EmelyanovAlexey@users.noreply.github.com>
2023-03-05 20:49:41 +0700

    test commit

--- src/main.py
+++ src/main.py
@@ -4,6 +4,8 @@ from PyQt6.QtWidgets import QApplication
 
 from DebugWindow import runWindow as debugWindow
 
+# test commit
+
 if __name__ == '__main__':
     app = QApplication([])
     debugWindow()
